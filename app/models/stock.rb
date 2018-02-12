a677fc14122b65997ba750319f65b97a1f6ced29
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-11 20:32:44 -0800

    Added associations between users and stocks, user_stocks routes, functionality to add a stock to user's portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -13,5 +16,9 @@ class Stock < ActiveRecord::Base
 
   def self.strip_commas(number)
     number.gsub(',', '') # replace comma with nothing
+  end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
