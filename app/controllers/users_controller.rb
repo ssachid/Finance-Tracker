f179fd2495bf8eccaa741db4419038480f754073
Shambhavi Sachid <shambu1985@gmail.com>
2018-02-11 22:46:16 -0800

    created stocks listing view along with destroy action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
+    @user_stocks = @user.stocks
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  def stock_already_added?(ticker_symbol)
+    # first find the stock in the stocks table
+    stock = Stock.find_by_ticker(ticker_symbol)
+
+    # return false if the stock is not present
+    return false unless stock
+
+    # check if the stock is already tracked by the user
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    # check if the user is under their stock limit of 10
+    user_stocks.count < 10
+  end
+
+  def can_add_stock?(ticker_symbol)
+    # if the user's stock limit is less than 10
+    # and the stock is not already added
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+
 end
